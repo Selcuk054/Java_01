b8dc9da0c89ba6aa21b67ac5404c5d4e3cee95bc
Selcuk054 <107318967+Selcuk054@users.noreply.github.com>
2022-06-21 14:37:13 +0200

    Add files via upload

---
+++ day02_variables/C01_Variables.java
@@ -0,0 +1,30 @@
+package day02_variables;
+
+public class C01_Variables {
+    public static void main(String[] args) {
+            int            not              =                   60                   ;
+        //data turu   variables ismi    assignment sign      variables degeri     islem bitti isareti:)
+
+            int not2 =            70;
+            //variables          deger
+        //java once degeri hesaplar sonra assign islemini yapar
+
+        //not2 yi 80 yapalim
+
+        not2=80;
+        int not3=100;
+
+        not3=900;
+        //Burada basina tekrar int yazmiyoruz (cocuk dogmus birdaha dogmaz :)))
+
+        //yeni bir int variables olusturalim degeri ilk iki variable in ortalamasi olsun.
+
+        int ort=(not +not2+not3)/3 ;
+
+        System.out.println(ort);
+        /*Cift tirnak icinde yazilen metindir bunlarin icinde ne goruse o sekilde yazdirir,
+        eger bir variable'nin degerini yazdirmak istiyorsak cift tirnak(double quotes) kullanmadan yazmaliyiz
+        */
+
+    }
+}
---
+++ day02_variables/C02_Variables.java
@@ -0,0 +1,42 @@
+package day02_variables;
+
+public class C02_Variables {
+    public static void main(String[] args) {
+        //Class Work 1
+        int sayi=10;
+        boolean guzelMi=true;
+        char sayim='3';
+        System.out.println(sayi);
+        System.out.println("sayi");
+        System.out.println("sayi : " + sayi); //sayi:10
+
+         //Class Work 2
+        String isminiz= "Selcuk", soyIsminiz= "Kardes";
+
+        System.out.println("Isminiz : "+ isminiz);
+        System.out.println("Soy isminiz : "+ soyIsminiz);
+
+        //Class Work 3
+        byte yas=76;
+        short yil=2022;
+        System.out.println(yas + yil);
+
+        //Class Work 4
+
+        long nufus = 23456;
+        double yasOrani= 76.234;
+        System.out.println(nufus + yasOrani);
+
+        // Class Work 5
+
+        char numberOfHardware= '9';
+        System.out.println( numberOfHardware);
+
+        //Class Work 6
+        int coronaPositive= 345987345;
+        char numberOfCountries= '2';
+        System.out.println(coronaPositive + numberOfCountries);
+        System.out.println(numberOfCountries + numberOfCountries);
+
+    }
+}
